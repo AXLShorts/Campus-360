e7b3d597d43318c8a052e5d7f5f250dbd43842d8
AXL <116296322+AXLShorts@users.noreply.github.com>
2023-05-24 21:38:45 +0500

    Added scrolling and accordions

--- script.js
+++ script.js
@@ -12,6 +12,14 @@ document.getElementById("next2").onclick = function () {
   );
 };
 
+document.getElementById("next3").onclick = function () {
+  changeIframe(
+    "iframe-con",
+    "https://momento360.com/e/u/6c588a40a2a348d4a80e6df5014b873c?utm_campaign=embed&utm_source=other&heading=-9.78&pitch=6.42&field-of-view=50.85&size=medium&display-plan=true"
+    //replace this with image for the fourth accordion
+  );
+};
+
 document.getElementById("prev1").onclick = function () {
   changeIframe(
     "iframe-con",
@@ -23,6 +31,13 @@ document.getElementById("prev2").onclick = function () {
   changeIframe(
     "iframe-con",
     "https://momento360.com/e/u/e533781537b64b9fa64fb70c2423ad05?utm_campaign=embed&utm_source=other&heading=323.13&pitch=2.74&field-of-view=75&size=medium&display-plan=true"
+  );
+};
+
+document.getElementById("prev3").onclick = function () {
+  changeIframe(
+    "iframe-con",
+    "https://momento360.com/e/u/076bf5bb8cff4eba9fef4988b1f1d03e?utm_campaign=embed&utm_source=other&heading=314.2&pitch=-5.08&field-of-view=75&size=medium&display-plan=true"
   );
 };
 
